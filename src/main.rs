079042248d267d3cc1c1caea0ad9bc4b07b82cfc
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-10 22:35:43 +0900

    Rename todo to task

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 pub mod domain {
-    pub mod todo;
+    pub mod task;
 }
 
-use domain::todo;
+use domain::task;
 
 fn main() {
-    let t1 = todo::Task {
+    let t1 = task::Task {
         title: String::from("Hoge"),
     };
-    let t2 = todo::Task {
+    let t2 = task::Task {
         title: String::from("Fuga"),
     };
-    let mut l = todo::TaskList { list: vec![t1] };
+    let mut l = task::TaskList { list: vec![t1] };
     l.add(t2);
     println!("{:?}", l);
 }
