e36fed05f50b20d236147b64d3cb5bc47a43eec2
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-10 23:10:24 +0900

    Move to cfg(test)

--- src/application/task_service.rs
+++ src/application/task_service.rs
@@ -1,6 +1,5 @@
 use crate::application::task_repository::TaskRepository;
-use crate::domain::task::{Task, TaskList};
-use crate::infrastructure::task_repository_impl::TaskRepositoryOnMemory;
+use crate::domain::task::TaskList;
 
 pub struct TaskService<R: TaskRepository> {
     task_repository: R,
@@ -16,29 +15,36 @@ impl<R: TaskRepository> TaskService<R> {
     }
 }
 
-#[test]
-fn test_fetch_all() {
-    let repository = TaskRepositoryOnMemory::new(TaskList {
-        list: vec![
-            Task {
-                title: String::from("Hoge"),
-            },
-            Task {
-                title: String::from("Fuga"),
-            },
-        ],
-    });
-    let service = TaskService::new(repository);
-    let actual = service.fetch_all();
-    let expected = TaskList {
-        list: vec![
-            Task {
-                title: String::from("Hoge"),
-            },
-            Task {
-                title: String::from("Fuga"),
-            },
-        ],
-    };
-    assert_eq!(*actual, expected);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::task::Task;
+    use crate::infrastructure::task_repository_impl::TaskRepositoryOnMemory;
+
+    #[test]
+    fn test_fetch_all() {
+        let repository = TaskRepositoryOnMemory::new(TaskList {
+            list: vec![
+                Task {
+                    title: String::from("Hoge"),
+                },
+                Task {
+                    title: String::from("Fuga"),
+                },
+            ],
+        });
+        let service = TaskService::new(repository);
+        let actual = service.fetch_all();
+        let expected = TaskList {
+            list: vec![
+                Task {
+                    title: String::from("Hoge"),
+                },
+                Task {
+                    title: String::from("Fuga"),
+                },
+            ],
+        };
+        assert_eq!(*actual, expected);
+    }
 }
